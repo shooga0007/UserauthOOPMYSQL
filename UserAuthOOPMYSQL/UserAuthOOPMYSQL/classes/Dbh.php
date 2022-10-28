038f5e3fdccd30976903560be535310a8429e08e
Jeremiah Ezarafe <116135420+shooga0007@users.noreply.github.com>
2022-10-27 23:53:51 -0700

    Update Dbh.php

--- UserAuthOOPMYSQL/UserAuthOOPMYSQL/classes/Dbh.php
+++ UserAuthOOPMYSQL/UserAuthOOPMYSQL/classes/Dbh.php
@@ -1,3 +1,21 @@
 <?php
+class Dbh{
+
+    private $hostname;
+    private $username;
+    private $pasword;
+    private $dbname;
+
+    public function connect(){
+        $this->hostname = "127.0.0.1";
+        $this->username = "root";
+        $this->password = "";
+        $this->dbname = "zuriphp";
+
+        $conn = new mysqli($this->hostname, $this->username,$this->passsword, $this->dbname);
+        return $conn;
+
+    }
+}
 
 
